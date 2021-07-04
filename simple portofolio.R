299ea260cfaab5acf12b93f5c58a8b7a19d73b3f
Mufidz <rmufidianto@outlook.com>
2021-07-04 07:03:00 +0700

    ver 1.1

--- simple portofolio.R
+++ simple portofolio.R
@@ -9,12 +9,11 @@ ui <- fluidPage(
       mainPanel(
         h4(strong("Hi disana, saya Petrik.")),
         br(),
-        p("Nama : Petrik"),
-        p("NIM : 20190801102"),
-        p("Fakultas : Ilmu Komputer"),
-        p("Jurusan : Teknik Informatika"),
-        p("Asal : Bekasi"),
-        br()
+        p(HTML("Nama     : Petrik"), style="white-space:pre-wrap"),
+        p(HTML("NIM      : 20190801102"), style="white-space:pre-wrap"),
+        p(HTML("Fakultas : Ilmu Komputer"), style="white-space:pre-wrap"),
+        p(HTML("Jurusan  : Teknik Informatika"), style="white-space:pre-wrap"),
+        p(HTML("Asal     : Bekasi"), style="white-space:pre-wrap")
       )
     )
 )
